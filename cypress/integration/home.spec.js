cc09e5a38b3fa9ce251b1e80888b9ea0ac87dc76
Jefferson Mariano de Souza <jefferson.msouza86@gmail.com>
2021-11-27 22:32:47 -0300

    adjusts selector

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,6 +1,6 @@
 describe('home page', () => {
   it('should make sure app is online', () => {
     cy.visit('https://buger-eats.vercel.app');
-    cy.get('h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats');
+    cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats');
   });
 });
