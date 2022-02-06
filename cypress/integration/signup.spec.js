7708cdcd7eae17cb70be6116a97e8e2ee7b71b78
Jefferson Mariano de Souza <jefferson.msouza86@gmail.com>
2022-02-05 22:16:45 -0300

    using pageobjects

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,10 +1,7 @@
+import SignUpPage from '../pages/SignUpPage';
+
 describe('Sign up', () => {
   it('User must be a deliveryman', () => {
-    cy.visit('https://buger-eats.vercel.app');
-    cy.get('a[href="/deliver"]').click();
-
-    cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas');
-
     const deliveryman = {
       name: 'John Doe',
       cpf: '00000014141',
@@ -22,30 +19,44 @@ describe('Sign up', () => {
       file: 'images/user-document.jpeg',
     };
 
-    cy.get('input[name="name"]').type(deliveryman.name);
-    cy.get('input[name="cpf"]').type(deliveryman.cpf);
-    cy.get('input[name="email"]').type(deliveryman.email);
-    cy.get('input[name="whatsapp"]').type(deliveryman.whatsapp);
+    const signUpPage = new SignUpPage();
 
-    cy.get('input[name="postalcode"]').type(deliveryman.address.postalcode);
-    cy.get('input[type="button"][value="Buscar CEP"]').click();
-
-    cy.get('input[name="address-number"]').type(deliveryman.address.number);
-    cy.get('input[name="address-details"]').type(deliveryman.address.details);
-
-    cy.get('input[name="address"]').should('have.value', deliveryman.address.street);
-    cy.get('input[name="district"]').should('have.value', deliveryman.address.district);
-    cy.get('input[name="city-uf"]').should('have.value', deliveryman.address.city_st);
-
-    cy.contains('.delivery-method li', deliveryman.delivery_method).click();
-
-    cy.get('input[type="file"][accept^="image"]').attachFile(deliveryman.file);
-
-    cy.get('button[type="submit"]').click();
+    signUpPage.go();
+    signUpPage.fillForm(deliveryman);
+    signUpPage.submit();
 
     const expectedMessage =
       'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.';
 
-    cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage);
+    signUpPage.modalContentShouldBe(expectedMessage);
+  });
+
+  it('User should see error when using invalid document', () => {
+    const deliveryman = {
+      name: 'John Doe',
+      cpf: '000000141AA',
+      email: 'john@doe.com',
+      whatsapp: '11999999999',
+      address: {
+        postalcode: '04534011',
+        street: 'Rua Joaquim Floriano',
+        number: '1000',
+        details: 'Ap 443',
+        district: 'Itaim Bibi',
+        city_st: 'São Paulo/SP',
+      },
+      delivery_method: 'Moto',
+      file: 'images/user-document.jpeg',
+    };
+
+    const signUpPage = new SignUpPage();
+
+    signUpPage.go();
+    signUpPage.fillForm(deliveryman);
+    signUpPage.submit();
+
+    const expectedMessage = 'Oops! CPF inválido';
+
+    signUpPage.alertMessageShouldBe(expectedMessage);
   });
 });
---
+++ cypress/pages/SignUpPage.js
@@ -0,0 +1,43 @@
+class SignUpPage {
+  go() {
+    cy.visit('https://buger-eats.vercel.app');
+    cy.get('a[href="/deliver"]').click();
+
+    cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas');
+  }
+
+  fillForm(deliveryman) {
+    cy.get('input[name="name"]').type(deliveryman.name);
+    cy.get('input[name="cpf"]').type(deliveryman.cpf);
+    cy.get('input[name="email"]').type(deliveryman.email);
+    cy.get('input[name="whatsapp"]').type(deliveryman.whatsapp);
+
+    cy.get('input[name="postalcode"]').type(deliveryman.address.postalcode);
+    cy.get('input[type="button"][value="Buscar CEP"]').click();
+
+    cy.get('input[name="address-number"]').type(deliveryman.address.number);
+    cy.get('input[name="address-details"]').type(deliveryman.address.details);
+
+    cy.get('input[name="address"]').should('have.value', deliveryman.address.street);
+    cy.get('input[name="district"]').should('have.value', deliveryman.address.district);
+    cy.get('input[name="city-uf"]').should('have.value', deliveryman.address.city_st);
+
+    cy.contains('.delivery-method li', deliveryman.delivery_method).click();
+
+    cy.get('input[type="file"][accept^="image"]').attachFile(deliveryman.file);
+  }
+
+  submit() {
+    cy.get('button[type="submit"]').click();
+  }
+
+  modalContentShouldBe(expectedMessage) {
+    cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage);
+  }
+
+  alertMessageShouldBe(expectedMessage) {
+    cy.get('.alert-error').should('have.text', expectedMessage);
+  }
+}
+
+export default SignUpPage;
