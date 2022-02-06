059aa76d4c190d4d0d53d78079d1b052031d88d4
Jefferson Mariano de Souza <jefferson.msouza86@gmail.com>
2022-02-05 22:48:13 -0300

    using fixtures for datasource

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,26 +1,17 @@
 import signUpPage from '../pages/SignUpPage';
 
 describe('Sign up', () => {
-  it('User must be a deliveryman', () => {
-    const deliveryman = {
-      name: 'John Doe',
-      cpf: '00000014141',
-      email: 'john@doe.com',
-      whatsapp: '11999999999',
-      address: {
-        postalcode: '04534011',
-        street: 'Rua Joaquim Floriano',
-        number: '1000',
-        details: 'Ap 443',
-        district: 'Itaim Bibi',
-        city_st: 'São Paulo/SP',
-      },
-      delivery_method: 'Moto',
-      file: 'images/user-document.jpeg',
-    };
+  let deliveryman = null;
 
+  before(() => {
+    cy.fixture('deliveryman').then((data) => {
+      deliveryman = data;
+    });
+  });
+
+  it('User must be a deliveryman', () => {
     signUpPage.go();
-    signUpPage.fillForm(deliveryman);
+    signUpPage.fillForm(deliveryman.signup);
     signUpPage.submit();
 
     const expectedMessage =
@@ -30,25 +21,8 @@ describe('Sign up', () => {
   });
 
   it('User should see error when using invalid document', () => {
-    const deliveryman = {
-      name: 'John Doe',
-      cpf: '000000141AA',
-      email: 'john@doe.com',
-      whatsapp: '11999999999',
-      address: {
-        postalcode: '04534011',
-        street: 'Rua Joaquim Floriano',
-        number: '1000',
-        details: 'Ap 443',
-        district: 'Itaim Bibi',
-        city_st: 'São Paulo/SP',
-      },
-      delivery_method: 'Moto',
-      file: 'images/user-document.jpeg',
-    };
-
     signUpPage.go();
-    signUpPage.fillForm(deliveryman);
+    signUpPage.fillForm(deliveryman.invalidDocument);
     signUpPage.submit();
 
     const expectedMessage = 'Oops! CPF inválido';
